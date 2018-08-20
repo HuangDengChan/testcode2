5e88fa9caf6c0f4ded18c2200e6c113106fa8872
HuangDengChan <40771152+HuangDengChan@users.noreply.github.com>
2018-08-20 15:57:56 +0800

    Update one.js

--- one.js
+++ one.js
@@ -9,7 +9,7 @@ const regtest = bitcoin.networks.testnet
 const bip65 = require('bip65')
 
 //輸入私鑰，教學用，請勿隨意公開私鑰
-var privateKey = "cNcvQphXWjAJ365Y8Tuhti5a6fCh9ftpR3GScZs5vdidBwQqpxT4";
+var privateKey = "cRPWcQLiTgW6SBp6Jqj9nThPfbU9BKZKheqsrxBfQckEWgDp8Rw3";
 
 //產生公鑰跟私鑰
 const keyPair = bitcoin.ECPair.fromWIF(privateKey,regtest);
@@ -21,10 +21,10 @@ const txb = new bitcoin.TransactionBuilder(regtest);
 
 
 //用listunspent 取出最後一筆資料的txid
-txb.addInput('c3acac43c6f0e83538fb5c82ed01379349f5dccf43a8af7bb5d8f0c60fb1fcfc', 0);
+txb.addInput('a0da774fc841f5d2d7acfafc13d6b1014296b575d5334fe8a4c9a30054355fff', 0);
 
 //用getnewaddress取得新的位址
-txb.addOutput('2MxfNS8sB1GcCo3GBLmX2PUw277H9zvrn2L', 12)
+txb.addOutput('2MymL2zce7z8Xa2S8tLaZtxBD4cCiyXman5', 12)
 
 //交易簽名
 txb.sign(0, keyPair); //第一個位置的是上一筆交易中的第一個支出，第二個欄位是我們的公鑰與私鑰
